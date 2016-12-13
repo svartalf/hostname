00b81773439d06348316198b58128aef22a373ee
fengcen <fengcen.love@gmail.com>
2016-12-13 20:34:06 +0800

    init repository.

---
+++ README.md
@@ -0,0 +1,33 @@
+
+hostname
+===
+[![docs](https://docs.rs/hostname/badge.svg?version=0.1.0 "docs")](https://docs.rs/hostname)
+
+Get hostname. Compatible with windows and unix.
+
+## [Document](https://docs.rs/hostname)
+
+## Usage
+Add dependency to Cargo.toml
+
+```toml
+[dependencies]
+hostname = "^0.1"
+```
+
+In your `main.rs` or `lib.rs`:
+
+```rust
+extern crate hostname;
+```
+
+## Examples
+```
+use hostname::get_hostname;
+
+assert!(!get_hostname().is_ok());
+```
+
+## License
+hostname is primarily distributed under the terms of the MIT license.
+See [LICENSE](LICENSE) for details.
---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+//! Get hostname. Compatiable with windows and linux.
+//!
+//! # Examples
+//! ```
+//! extern crate hostname;
+//!
+//! assert!(hostname::get_hostname().is_some());
+//! ```
+//!
+#![cfg_attr(all(feature = "unstable", test), feature(test))]
+
+#[cfg(windows)]
+extern crate winutil;
+
+#[cfg(unix)]
+extern crate libc;
+
+
+/// Get hostname.
+#[cfg(windows)]
+pub fn get_hostname() -> Option<String> {
+    winutil::get_computer_name()
+}
+
+
+#[cfg(unix)]
+extern "C" {
+    pub fn gethostname(name: *mut libc::c_char, size: libc::size_t) -> libc::c_int;
+}
+
+#[cfg(unix)]
+use std::ffi::CStr;
+
+/// Get hostname.
+#[cfg(unix)]
+pub fn get_hostname() -> Option<String> {
+    let len = 255;
+    let mut buf = Vec::<u8>::with_capacity(len);
+    let ptr = buf.as_mut_ptr() as *mut libc::c_char;
+
+    unsafe {
+        if gethostname(ptr, len as libc::size_t) != 0 {
+            return None;
+        }
+
+        Some(CStr::from_ptr(ptr).to_string_lossy().into_owned())
+    }
+}
+
+#[test]
+fn test_get_hostname() {
+    assert!(get_hostname().is_some());
+    assert!(!get_hostname().unwrap().is_empty());
+}
+
+#[cfg(all(feature = "unstable", test))]
+mod benches {
+    extern crate test;
+    use super::get_hostname;
+
+    #[bench]
+    fn bench_get_hostname(b: &mut test::Bencher) {
+        b.iter(|| get_hostname().unwrap())
+    }
+}
